adfb125cca833b37d1aca898f03aa95913513790
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-08-02 07:30:23 +0300

    Hash for Two Sum

--- TowSum.py
+++ TowSum.py
@@ -2,13 +2,27 @@ nums = [2, 7, 11, 15]
 target = 9
 
 
-
+'''
 class Solution: 
     def twoSum( self , nums : list[int]  , target : int ) -> list [int] :
         for  i , num in enumerate(nums ):
             for j in range ( i + 1  , len(nums)):
-                if num + nums[j] == target :
-                    return [i , j]
+              if num + nums[j] == target :
+                print([i , j])
+                return [i , j]
+'''            
+
+
+class Solution: 
+    def twoSum( self , nums : list[int]  , target : int ) -> list [int] :
+        hash= {}
+        for i , num in enumerate(nums):
+            if target -num  in hash:
+                return [i , hash[target- num]]
+            hash[num ] =i 
+    
+
+                    
 sol = Solution()
 
-sol.towSum( target ,nums)
+sol.twoSum( nums ,target)
