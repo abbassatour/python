b2c92e7a520f1afcc4add9a61ba4cf802aa72690
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-08-18 19:45:26 +0300

    pythonic

--- 268.MissingNumber.py
+++ 268.MissingNumber.py
@@ -1,3 +1,4 @@
+#mine 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         numbers = set(nums)
@@ -7,3 +8,16 @@ class Solution:
                 return n
             n+=1
         return len(nums)
+
+#more pythonic way
+# Instead of:
+# n = 0
+# while n < len(nums):
+#     if n not in numbers:
+#         return n
+#     n += 1
+
+# More Pythonic:
+for num in range(len(nums) + 1):
+    if num not in numbers:
+        return num
