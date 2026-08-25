10aab06a9da04e5d887bd48d061db0cc3c2928cc
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-08-25 15:59:15 +0300

    recursion . dont forgate about self.

--- 326.PowerOfThree.py
+++ 326.PowerOfThree.py
@@ -13,3 +13,17 @@ class Solution:
         if n !=3: 
             return False
         return True 
+
+#Recursion
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n == 1 : 
+            return True
+        if n < 3 : 
+            return False 
+        
+        if n % 3 != 0 : 
+            return False
+        
+        
+        return self.isPowerOfThree(n // 3)
