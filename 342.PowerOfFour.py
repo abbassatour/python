a2415f78e399fd2a8cf2c573a0802b9c7ef6c5a7
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-08-27 20:47:24 +0300

    342.Tow Solutions for revenge

--- 342.PowerOfFour.py
+++ 342.PowerOfFour.py
@@ -2,3 +2,24 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         return n > 0 and (n & (n - 1)) == 0 and (n % 3 == 1)
+
+#normal loop 
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        complications = set()
+        for i in range(16): 
+            complications.add(4** i)
+        
+        return  n in complications
+
+
+
+#recursion
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n<=0 :
+            return False
+        if n ==1: 
+            return True
+        
+        return  self.isPowerOfFour(n/4)
