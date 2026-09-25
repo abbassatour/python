2ddbdb46bd87d4e38b9a539061f3081610fcbed6
satwrabbas@gmail.com <195249653+satwrabbas@users.noreply.github.com>
2026-09-25 09:54:49 +0300

    Arrays

---
+++ Array1.Q2.ShuffleTheArray.py
@@ -0,0 +1,9 @@
+mine
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        ans = []
+        for i in range(n): 
+            ans.append(nums[i])
+            ans.append(nums[n])
+            n+= 1 
+        return ans
